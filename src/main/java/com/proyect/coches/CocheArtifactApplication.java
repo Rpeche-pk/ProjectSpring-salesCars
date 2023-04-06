8691a8cc42130b52a64feb034f37451cfc8e293d
Rpeche-pk <pecheaparcana1998@gmail.com>
2023-04-05 22:03:34 -0500

    start project spring boot

---
+++ src/main/java/com/proyect/coches/CocheArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocheArtifactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocheArtifactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyect/coches/CocheArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.coches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocheArtifactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
